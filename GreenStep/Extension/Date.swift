2da6cb3c988e82c80ed6573e1976979b4934d619
JIN <jin724596@gmail.com>
2025-07-30 22:47:22 +0900

    Date Formatter 추가

--- GreenStep/Extension/Date.swift
+++ GreenStep/Extension/Date.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+extension Date {
+    
+    init(lastUpdated: String){
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        formatter.timeZone = TimeZone(identifier: "Asia/Seoul")
+        let date = formatter.date(from: lastUpdated) ?? Date()
+        self.init(timeInterval: 0, since: date)
+    }
+    
+    private var shortDateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US")
+        formatter.dateFormat = "yyyy-MMM-dd"
+        return formatter
+    }
+    
+    func asShortDateString() -> String {
+        return shortDateFormatter.string(from: self)
+    }
+}
